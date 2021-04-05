8464847d37911e5e650c59d4fbca96a7db7db4b9
Thi Nguyen <nguyentamthi11@gmail.com>
2021-04-05 09:11:06 +0700

    Add dvelop up/down script

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Bondle shorten url
+
+## Requirements:
+- Node: v14.15.4 
+- Yarn: 1.22.10
+- Docker: 20.10.5
+- Docker-Compose: 1.28.5
+## Start local
+
+1. Service up
+```bash
+develop/up
+```
+2. Service down
+```bash
+develop/down
+```
--- database/initdb/create_tables.sql
+++ database/initdb/create_tables.sql
@@ -1,13 +1,13 @@
 create table url
 (
-    id          bigserial not null constraint url_pkprimary key,
+    id          bigserial not null constraint url_pk primary key,
     long_url    varchar(255),
     hash        varchar(100),
+    uuid        varchar(100),
     created_at  timestamp with time zone default now(),
     created_by  varchar(255)             default 'ANONYMOUS'::character varying,
     modified_at timestamp with time zone default now(),
-    modified_by varchar(255)             default 'ANONYMOUS'::character varying,
-    uuid        varchar(100)
+    modified_by varchar(255)             default 'ANONYMOUS'::character varying
 );
 
 create unique index url_id_uindex
